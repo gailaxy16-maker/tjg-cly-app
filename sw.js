6e9a2576c28d4ae08abad5741116e65b578a3825
TJG <tjg@local>
2026-07-01 00:39:05 +0700

    Lift search box off the page background so it reads as tappable, not locked

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'tjg-cly-v4';
+const CACHE = 'tjg-cly-v5';
 const ASSETS = ['./', './index.html', './manifest.json', './icons/icon-192.png', './icons/icon-512.png'];
 
 self.addEventListener('install', e => {
